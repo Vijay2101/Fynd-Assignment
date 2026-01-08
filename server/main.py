9221d65a6d85141a8d184e107f64e88a2b6d6bfe
Vijay Kumar <kumarvijay2003.vk@gmail.com>
2026-01-08 21:19:21 +0530

    Enable credentials in CORS middleware

--- server/main.py
+++ server/main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="AI Feedback System")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=False,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
